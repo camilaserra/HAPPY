679a4000430b75e60807ab42dd1b58809d2a8b7b
Camila Serra <camilaserrasilva@gmail.com>
2020-10-28 18:41:39 -0300

    form complete

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -32,3 +32,64 @@ map.on('click', (event) => {
         marker = L.marker([lat,lng], {icon})
         .addTo(map) 
 })
+
+
+
+//adicionar o campo de fotos.
+function addPhotoField() {
+    //pegar o container de fotos #images
+    const container = document.querySelector('#images')
+    //pegar o container para duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    //realizar o clone da última imagem adicionada.
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    
+    //verificar se o campo esta vazio antes de adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+
+    if(input.value =="") {
+        return
+    }
+    //limpar o campo antes de adicionar ao container de imagens
+    input.value = ""
+    //adicionar o clone ao container de #imagens
+    container.appendChild(newFieldContainer)
+}
+
+
+// function delet filed
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2) {
+        //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove();
+}
+
+//select yes or no
+function toggleSelect(event){
+    
+    //retirar a class .active (dos botões)
+    document.querySelectorAll('.button-select button')
+    .forEach( function(button){
+        button.classList.remove('active')
+    })
+
+    //colocar a class .active nesse botao clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    //atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    //verificar se sim ou não 
+    input.value = button.dataset.value
+
+}
